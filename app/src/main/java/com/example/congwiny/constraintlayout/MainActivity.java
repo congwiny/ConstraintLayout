6425f3ad907e283a6efacfb1c9e88384b0bcf8bb
congwiny <congwiny@newborn-town.com>
2017-10-31 18:28:53 +0800

    Add: modify constraint set

--- app/src/main/java/com/example/congwiny/constraintlayout/MainActivity.java
+++ app/src/main/java/com/example/congwiny/constraintlayout/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.congwiny.constraintlayout;
 
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.support.transition.TransitionManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintSet constraintSet1;
+    private ConstraintSet constraintSet2;
+    private ConstraintLayout constraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_constraint_set);
+
+        constraintSet1 = new ConstraintSet();
+        constraintSet2 = new ConstraintSet();
+        constraintLayout = (ConstraintLayout) findViewById(R.id.constraint_root);
+        //使用ConstraintSet拷贝ConstraintLayout中的约束关系
+        constraintSet1.clone(constraintLayout);
+        constraintSet2.clone(constraintLayout);
+    }
+
+    /**
+     * 重置ConstraintLayout
+     */
+    public void reset(View view) {
+        //使用Transition动画，改变布局
+        TransitionManager.beginDelayedTransition(constraintLayout);
+        constraintSet1.applyTo(constraintLayout);
+    }
+
+    public void change(View view) {
+        //clear 清除所有的约束关系，包括margin,padding,width,height都设置为0
+        //constraintSet2.clear(...);
+
+        TransitionManager.beginDelayedTransition(constraintLayout);
+        //清单文件中 supportsRtl,这里设置为start
+        constraintSet2.setMargin(R.id.button1, ConstraintSet.START, 30);
+        constraintSet2.applyTo(constraintLayout);
+
     }
 }
